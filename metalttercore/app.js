9d351bb74d7b2c5247eebcb1643b9a5c0a16a501
Claudia Hernández <claudiacarril8@gmail.com>
2023-01-31 20:58:30 +0100

    installing Mongoose

--- metalttercore/app.js
+++ metalttercore/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Connecting to Mongoose
+// require('.lib/connectMongoose')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
---
+++ metalttercore/lib/connectMongoose.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+// Events
+mongoose.connection.on('error', err => {
+    console.log('Error de conexión a MongoDB', err)
+    process.exit(1)
+})
+
+mongoose.connection.once('open', () => {
+    console.log('Conectado a MongoDB en', mongoose.connection.name)
+})
+
+mongoose.connect('mongodb://127.0.0.1/nodepop')
+
+module.exports = mongoose.connection
